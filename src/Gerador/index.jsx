e9aae3f33211698463d02e097f12dca442abd573
Lucas Duarte <l.duarte.mk@gmail.com>
2020-09-02 02:16:56 -0300

    finished

--- src/Gerador/index.jsx
+++ src/Gerador/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { changeCommaToPoint } from "../Utils";
 
+import "./style.css";
+
 class Gerador extends React.Component {
   constructor() {
     super();
@@ -39,57 +41,70 @@ class Gerador extends React.Component {
 
   render() {
     return (
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <label htmlFor="inputNome">Nome do Produto</label>
-        <input
-          id="inputNome"
-          onChange={(e) => this.handleComponentState(e, "name")}
-          value={this.state.name}
-        />
+      <div className="main">
+        <section className="form">
+          <label htmlFor="inputNome">Nome do Produto</label>
+          <input
+            id="inputNome"
+            onChange={(e) => {
+              e.target.value.length < 18 &&
+                this.handleComponentState(e, "name");
+            }}
+            value={this.state.name}
+          />
 
-    <label htmlFor="inputValue">Valor do Produto do produto</label>
-        <input
-          id="inputValue"
-          onChange={(e) => this.handleComponentState(e, "value")}
-          value={this.state.value}
-        />
+          <label htmlFor="inputValue">Valor do Produto do produto</label>
+          <input
+            id="inputValue"
+            onChange={(e) => this.handleComponentState(e, "value")}
+            value={this.state.value}
+          />
 
-        <label htmlFor="inputRate">Taxa (% a.m)</label>
-        <input
-          type="text"
-          id="inputRate"
-          onChange={(e) => this.handleComponentState(e, "rate")}
-          value={this.state.rate}
-        />
+          <label htmlFor="inputRate">Taxa (% a.m)</label>
+          <input
+            type="text"
+            id="inputRate"
+            onChange={(e) => this.handleComponentState(e, "rate")}
+            value={`${this.state.rate}`}
+          />
 
-        <label htmlFor="inputInstallments">
-          Quantidade de parcelas (12 máximo)
-        </label>
-        <input
-          type="number"
-          id="inputInstallments"
-          onChange={(e) => {
-            e.target.value <= 12 &&
-              e.target.value > 0 &&
-              this.handleComponentState(e, "installments");
-          }}
-          value={this.state.installments}
-        />
+          <label htmlFor="inputInstallments">
+            Quantidade de parcelas (12 máximo)
+          </label>
+          <input
+            type="number"
+            id="inputInstallments"
+            onChange={(e) => {
+              e.target.value <= 12 &&
+                e.target.value > 0 &&
+                this.handleComponentState(e, "installments");
+            }}
+            value={this.state.installments}
+          />
+        </section>
 
-        <label>Simulação {`${this.state.name}`}</label>
-        {this.state.optionsList.map((opt) => (
-          <div key={opt}>
-            <div>
-              <span>Parcela nº {`${opt}`}</span>
-            </div>
-            <div>
-              <span>Valor R$ {this.getValueInstallment()}</span>
-            </div>
+        <section className="rightPanels">
+          <div className="simulacao">
+            <label className="title">Simulação {`${this.state.name}`}</label>
+            <span>
+              Total de: R${" "}
+              {(this.getValueInstallment() * this.state.installments).toFixed(2)}
+            </span>
           </div>
-        ))}
-        <div>
-          <span>No toal de: R$ {(this.getValueInstallment() * this.state.installments).toFixed(2)}</span>
-        </div>
+
+          <div>
+          {
+          this.state.rate &&
+              this.state.value &&
+              this.state.name &&
+              this.state.optionsList.map((opt) => (
+                <div className="iten" key={opt}>
+                    <span>Parcela nº {`${opt}`}</span>
+                    <span>Valor R$ {this.getValueInstallment()}</span>
+                </div>
+              ))}
+          </div>
+        </section>
       </div>
     );
   }
